8be6b483aa9a1dd6a0479cb22876c3967a2a9dfa
Kai Kohlsdorf <kai.w.kohlsdorf@gmail.com>
2022-06-17 14:51:06 -0700

    WIP js, css, html to work together to submit pizza pie choices with a cost correctly.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,48 @@
 //Business Logic
 
-function PizzaPie(toppings, size, cost) {
-  this.toppings = toppings;
-  this.size = size;
-  this.cost = cost;
-}
+// function PizzaPie(toppings, size, cost) {
+//   this.toppings = toppings;
+//   this.size = size;
+//   this.cost = cost;
+// }
 
-PizzaPie.prototype.clickedToppings = function() {
-  this.cost = this.cost + (this.toppings.length * 1.5);
-  return this.cost;
-};
+// PizzaPie.prototype.clickedToppings = function() {
+//   this.cost = this.cost + (this.toppings.length * 1.5);
+//   return this.cost;
+// };
 
-PizzaPie.prototype.clickedSize = function() {
-  if (this.size === "personal") {
-    this.cost += 8;
-  } else if (this.size === "medium") {
-    this.cost += 13;
-  } else {
-    this.cost += 16;
+// PizzaPie.prototype.clickedSize = function() {
+//   if (this.size === "personal") {
+//     this.cost += 8;
+//   } else if (this.size === "medium") {
+//     this.cost += 13;
+//   } else {
+//     this.cost += 16;
+//   }
+//     return this.cost;
+// };
+
+function PizzaPie(toppings, size) {
+    this.toppings = toppings;
+    this.size = size;
   }
+  
+  PizzaPie.prototype.clickedToppings = function() {
+    this.cost = this.cost + (this.toppings.length * 1.5);
     return this.cost;
-};
+  };
+  
+  PizzaPie.prototype.clickedSize = function() {
+    if (this.size === "personal") {
+      this.cost += 8;
+    } else if (this.size === "medium") {
+      this.cost += 13;
+    } else {
+      this.cost += 16;
+    }
+      return this.cost;
+  };
+
 
 // User Interface Logic
 
@@ -29,8 +51,21 @@ let pizzaPie = new PizzaPie();
 $(document).ready(function() {
   $("form#new-pizzaPie").submit(function(event) {
     event.preventDefault();
-    const inputtedTopping = $("input#new-topping").val();
-    const inputtedSize = $("input#new-size").val();
-    let newPizzaPie = new PizzaPie(inputtedTopping, inputtedSize);
+    $("#toppings").show();
+    $("input:checkbox[name=topping]:checked").each(function(){
+      const topping = $(this).val();
+      $('#toppings').append(topping + "<br>");
+    })
+    $("#size").show();
+    $("input:checkbox[name=size]:checked").each(function(){
+      const size = $(this).val();
+      $('#size').append(size + "<br>");
+    })
+
+    const show-pizzaPie = newPizzaPie.cost;
+
+    $("#show-pizzaPie").show();
+    $("#total").text(show-PizzaPie);
+
   })
 })
